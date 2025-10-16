22c8d350e5ffc8cc0b0db047bbd2fcd124db8d83
Frances2345 <francesco.moreno@tecsup.edu.pe>
2025-10-16 17:31:23 -0500

    a

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,16 +2,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    public GameObject jugador;
+    public float rangoPersecucion;
+    public Rigidbody2D rb;
+    public float velocidadMovimiento;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        if(jugador == null)
+        {
+            Debug.LogWarning("No existe un juegador");
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distanciaJugador = Mathf.Abs(jugador.transform.position.x - transform.position.x);
+
+        if(distanciaJugador < rangoPersecucion)
+        {
+            float directionX = Mathf.Sign(jugador.transform.position.x - transform.position.x);
+            Vector2 movimiento = new Vector2(directionX, 0) * velocidadMovimiento * Time.deltaTime;
+            rb.MovePosition(rb.position + movimiento);
+
+        }
     }
 }
