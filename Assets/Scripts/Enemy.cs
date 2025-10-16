1f06ac90619907dbd3fef0d50492a737a75c1a91
jeremymamanis-bit <jeremymamani832@gmail.com>
2025-10-16 18:23:10 -0500

    Nuevo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
-    public GameObject jugador;
+    public GameObject Player;
     public float rangoPersecucion;
     public Rigidbody2D rb;
     
@@ -10,24 +10,19 @@ public class Enemy : Entity
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        if(jugador == null)
+        if(Player == null)
         {
             Debug.LogWarning("No existe un juegador");
-
         }
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        float distanciaJugador = Mathf.Abs(jugador.transform.position.x - transform.position.x);
-
-        if(distanciaJugador < rangoPersecucion)
-        {
-            float directionX = Mathf.Sign(jugador.transform.position.x - transform.position.x);
-            Vector2 movimiento = new Vector2(directionX, 0) * MoveSpeed * Time.deltaTime;
-            rb.MovePosition(rb.position + movimiento);
-
-        }
+        
     }
+
+
+    
+
 }
---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public interface ISeguimiento
+{
+    void SeguirPlayer();
+}
+public interface IInteractuable
+{
+    public void Interact(GameObject observer);
+}
